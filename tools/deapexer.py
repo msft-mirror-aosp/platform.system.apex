msft-mirror-aosp/platform.system.apex

commit dab7600868d6d42282e14975a8169354fe16149b

deapexer: fix lint errors
Bug: n/a
Test: pylint tools/deapexer.py
Change-Id: I46b0566d1070d0f87a8fbd40621a7f0f745e9cae

tools/deapexer.py
@@ -46,16 +46,16 @@ FS_TYPES = [
 def RetrieveFileSystemType(file):
   """Returns filesystem type with magic"""
   with open(file, 'rb') as f:
-    for type, offset, magic in FS_TYPES:
+    for fs_type, offset, magic in FS_TYPES:
       buf = bytearray(len(magic))
       f.seek(offset, os.SEEK_SET)
       f.readinto(buf)
       if buf == magic:
-        return type
+        return fs_type
   raise ValueError('Failed to retrieve filesystem type')
 
 class ApexImageEntry(object):
-
+  """Represents an entry in APEX payload"""
   def __init__(self, name, base_dir, permissions, size, ino, extents,
                is_directory, is_symlink, security_context):
     self._name = name
@@ -126,21 +126,21 @@ class ApexImageEntry(object):
     else:
       ret += '-'
 
-    def mask_as_string(m):
+    def MaskAsString(m):
       ret = 'r' if m & 4 == 4 else '-'
       ret += 'w' if m & 2 == 2 else '-'
       ret += 'x' if m & 1 == 1 else '-'
       return ret
 
-    ret += mask_as_string(self._permissions >> 6)
-    ret += mask_as_string((self._permissions >> 3) & 7)
-    ret += mask_as_string(self._permissions & 7)
+    ret += MaskAsString(self._permissions >> 6)
+    ret += MaskAsString((self._permissions >> 3) & 7)
+    ret += MaskAsString(self._permissions & 7)
 
     return ret + ' ' + self._size + ' ' + self._name
 
 
 class ApexImageDirectory(object):
-
+  """Represents a directory entry in APEX payload"""
   def __init__(self, path, entries, apex):
     self._path = path
     self._entries = sorted(entries, key=lambda e: e.name)
@@ -154,11 +154,11 @@ class ApexImageDirectory(object):
           yield ce
 
   def enter_subdir(self, entry):
-    return self._apex._list(self._path + entry.name + '/')
+    return self._apex._list(self._path + entry.name + '/') # pylint: disable=protected-access
 
 
 class Apex(object):
-
+  """Represents an APEX file"""
   def __init__(self, args):
     self._debugfs = args.debugfs_path
     self._fsckerofs = args.fsckerofs_path
@@ -175,12 +175,12 @@ class Apex(object):
   def __enter__(self):
     return self
 
-  def __exit__(self, type, value, traceback):
+  def __exit__(self, ex_type, value, traceback):
     pass
 
   def list(self, is_recursive=False):
     if self._payload_fs_type not in ['ext4']:
-      sys.exit(f"{self._payload_fs_type} is not supported for `list`.")
+      sys.exit(f'{self._payload_fs_type} is not supported for `list`.')
 
     root = self._list('./')
     return root.list(is_recursive)
@@ -188,7 +188,7 @@ class Apex(object):
   def _list(self, path):
     if path in self._cache:
       return self._cache[path]
-    res = subprocess.check_output([self._debugfs, '-R', 'ls -l -p %s' % path, self._payload],
+    res = subprocess.check_output([self._debugfs, '-R', f'ls -l -p {path}', self._payload],
                                   text=True, stderr=subprocess.DEVNULL)
     entries = []
     for line in res.split('\n'):
@@ -208,7 +208,7 @@ class Apex(object):
       is_directory=bits[1]=='4'
 
       if not is_symlink and not is_directory:
-        stdout = subprocess.check_output([self._debugfs, '-R', 'dump_extents <%s>' % ino,
+        stdout = subprocess.check_output([self._debugfs, '-R', f'dump_extents <{ino}>',
                                           self._payload], text=True, stderr=subprocess.DEVNULL)
         # Output of dump_extents for an inode fragmented in 3 blocks (length and addresses represent
         # block-sized sections):
@@ -226,9 +226,9 @@ class Apex(object):
             length = min(int(tokens[-1]) * BLOCK_SIZE, left_length)
             left_length -= length
             extents.append((offset, length))
-          if (left_length != 0): # dump_extents sometimes fails to display "hole" blocks
+          if left_length != 0: # dump_extents sometimes fails to display "hole" blocks
             raise ValueError
-        except:
+        except: # pylint: disable=bare-except
           extents = [] # [] means that we failed to retrieve the file location successfully
 
       # get 'security.selinux' attribute
@@ -255,18 +255,18 @@ class Apex(object):
 
   def extract(self, dest):
     if self._payload_fs_type == 'erofs':
-      subprocess.run([self._fsckerofs, '--extract=%s' % (dest), '--overwrite', self._payload],
+      subprocess.run([self._fsckerofs, f'--extract={dest}', '--overwrite', self._payload],
                      stdout=subprocess.DEVNULL, check=True)
     elif self._payload_fs_type == 'ext4':
       # Suppress stderr without failure
       try:
-        subprocess.run([self._debugfs, '-R', 'rdump ./ %s' % (dest), self._payload],
+        subprocess.run([self._debugfs, '-R', f'rdump ./ {dest}', self._payload],
                        capture_output=True, check=True)
       except subprocess.CalledProcessError as e:
         sys.exit(e.stderr)
     else:
       # TODO(b/279688635) f2fs is not supported yet.
-      sys.exit(f"{self._payload_fs_type} is not supported for `extract`.")
+      sys.exit(f'{self._payload_fs_type} is not supported for `extract`.')
 
   @property
   def payload_fs_type(self) -> str:
@@ -277,7 +277,7 @@ def RunList(args):
   if GetType(args.apex) == ApexType.COMPRESSED:
     with tempfile.TemporaryDirectory() as temp:
       decompressed_apex = os.path.join(temp, 'temp.apex')
-      decompress(args.apex, decompressed_apex)
+      Decompress(args.apex, decompressed_apex)
       args.apex = decompressed_apex
 
       RunList(args)
@@ -287,7 +287,7 @@ def RunList(args):
     for e in apex.list(is_recursive=True):
       # dot(., ..) directories
       if not e.root and e.name in ('.', '..'):
-          continue
+        continue
       res = ''
       if args.size:
         res += e.size + ' '
@@ -302,8 +302,8 @@ def RunList(args):
 def RunExtract(args):
   if GetType(args.apex) == ApexType.COMPRESSED:
     with tempfile.TemporaryDirectory() as temp:
-      decompressed_apex = os.path.join(temp, "temp.apex")
-      decompress(args.apex, decompressed_apex)
+      decompressed_apex = os.path.join(temp, 'temp.apex')
+      Decompress(args.apex, decompressed_apex)
       args.apex = decompressed_apex
 
       RunExtract(args)
@@ -313,8 +313,8 @@ def RunExtract(args):
     if not os.path.exists(args.dest):
       os.makedirs(args.dest, mode=0o755)
     apex.extract(args.dest)
-    if os.path.isdir(os.path.join(args.dest, "lost+found")):
-      shutil.rmtree(os.path.join(args.dest, "lost+found"))
+    if os.path.isdir(os.path.join(args.dest, 'lost+found')):
+      shutil.rmtree(os.path.join(args.dest, 'lost+found'))
 
 class ApexType(enum.Enum):
   INVALID = 0
@@ -367,9 +367,10 @@ def RunDecompress(args):
 
   compressed_apex_fp = args.input
   decompressed_apex_fp = args.output
-  return decompress(compressed_apex_fp, decompressed_apex_fp)
+  return Decompress(compressed_apex_fp, decompressed_apex_fp)
 
-def decompress(compressed_apex_fp, decompressed_apex_fp):
+
+def Decompress(compressed_apex_fp, decompressed_apex_fp):
   if os.path.exists(decompressed_apex_fp):
     print("Output path '" + decompressed_apex_fp + "' already exists")
     sys.exit(1)
@@ -393,19 +394,24 @@ def main(argv):
   debugfs_default = None
   fsckerofs_default = None
   if 'ANDROID_HOST_OUT' in os.environ:
-    debugfs_default = '%s/bin/debugfs_static' % os.environ['ANDROID_HOST_OUT']
-    fsckerofs_default = '%s/bin/fsck.erofs' % os.environ['ANDROID_HOST_OUT']
-  parser.add_argument('--debugfs_path', help='The path to debugfs binary', default=debugfs_default)
-  parser.add_argument('--fsckerofs_path', help='The path to fsck.erofs binary', default=fsckerofs_default)
+    debugfs_default = os.path.join(os.environ['ANDROID_HOST_OUT'], '/bin/debugfs_static')
+    fsckerofs_default = os.path.join(os.environ['ANDROID_HOST_OUT'], '/bin/fsck.erofs')
+  parser.add_argument(
+      '--debugfs_path', help='The path to debugfs binary', default=debugfs_default)
+  parser.add_argument(
+      '--fsckerofs_path', help='The path to fsck.erofs binary', default=fsckerofs_default)
   # TODO(b/279858383) remove the argument
   parser.add_argument('--blkid_path', help='NOT USED')
 
   subparsers = parser.add_subparsers(required=True, dest='cmd')
 
-  parser_list = subparsers.add_parser('list', help='prints content of an APEX to stdout')
+  parser_list = subparsers.add_parser(
+      'list', help='prints content of an APEX to stdout')
   parser_list.add_argument('apex', type=str, help='APEX file')
-  parser_list.add_argument('--size', help='also show the size of the files', action="store_true")
-  parser_list.add_argument('--extents', help='also show the location of the files', action="store_true")
+  parser_list.add_argument(
+      '--size', help='also show the size of the files', action='store_true')
+  parser_list.add_argument(
+      '--extents', help='also show the location of the files', action='store_true')
   parser_list.add_argument('-Z', '--contexts',
                            help='also show the security context of the files',
                            action='store_true')

pylintrc
@@ -161,12 +161,6 @@ disable=abstract-method,
 # mypackage.mymodule.MyReporterClass.
 output-format=text
 
-# Put messages in a separate file for each module / package specified on the
-# command line instead of printing them on stdout. Reports (if any) will be
-# written in a file name "pylint_global.[txt|html]". This option is deprecated
-# and it will be removed in Pylint 2.0.
-files-output=no
-
 # Tells whether to display a full report or only the messages
 reports=no
 
@@ -271,7 +265,7 @@ generated-members=
 [FORMAT]
 
 # Maximum number of characters on a single line.
-max-line-length=80
+max-line-length=100
 
 # TODO(https://github.com/PyCQA/pylint/issues/3352): Direct pylint to exempt
 # lines made too long by directives to pytype.
@@ -284,12 +278,6 @@ ignore-long-lines=(?x)(
 # Allow the body of an if to be on the same line as the test if there is no
 # else.
 single-line-if-stmt=yes
-
-# List of optional constructs for which whitespace checking is disabled. `dict-
-# separator` is used to allow tabulation in dicts, etc.: {1  : 1,\n222: 2}.
-# `trailing-comma` allows a space between comma and closing bracket: (a, ).
-# `empty-line` allows space-only lines.
-no-space-check=
 
 # Maximum number of lines in a module
 max-module-lines=99999
@@ -442,6 +430,6 @@ valid-metaclass-classmethod-first-arg=mcs
 
 # Exceptions that will emit a warning when being caught. Defaults to
 # "Exception"
-overgeneral-exceptions=StandardError,
-                       Exception,
-                       BaseException
+overgeneral-exceptions=builtins.StandardError,
+                       builtins.Exception,
+                       builtins.BaseException

commit 2761cfe5deedb0d097c842c4c802e850a643801d

deapexer: remove '/' in os.path.join
An absolute path in os.path.join() ignores all preceding segments.

This is a regression bug intrduced in
dab7600868d6d42282e14975a8169354fe16149b

Bug: n/a
Test: $ deapexer extract <apex> <dir>
Change-Id: Ib8db23ccf5e57e6d86ef08ffbeecceac52744dd2

tools/deapexer.py
@@ -394,8 +394,8 @@ def main(argv):
   debugfs_default = None
   fsckerofs_default = None
   if 'ANDROID_HOST_OUT' in os.environ:
-    debugfs_default = os.path.join(os.environ['ANDROID_HOST_OUT'], '/bin/debugfs_static')
-    fsckerofs_default = os.path.join(os.environ['ANDROID_HOST_OUT'], '/bin/fsck.erofs')
+    debugfs_default = os.path.join(os.environ['ANDROID_HOST_OUT'], 'bin/debugfs_static')
+    fsckerofs_default = os.path.join(os.environ['ANDROID_HOST_OUT'], 'bin/fsck.erofs')
   parser.add_argument(
       '--debugfs_path', help='The path to debugfs binary', default=debugfs_default)
   parser.add_argument(