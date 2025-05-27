d97be07c4eb0dfcf3ce7e0db1c6eb0b932a942e9
Jooyung Han <jooyung@google.com>
2025-05-27 16:21:12 +0900

    apexd: use parallel version of AddPreInstalled
    Let's use parallel version always.
    
    Bug: 418668939
    Test: ApexTestCases
    Change-Id: I510a0f566002f59ab6e4f6b960aee19d120ab45f

--- apexd/apexd.cpp
+++ apexd/apexd.cpp
@@ -2143,8 +2143,7 @@ int OnBootstrap() {
   auto time_started = boot_clock::now();
 
   ApexFileRepository& instance = ApexFileRepository::GetInstance();
-  Result<void> status =
-      instance.AddPreInstalledApexParallel(gConfig->builtin_dirs);
+  Result<void> status = instance.AddPreInstalledApex(gConfig->builtin_dirs);
   if (!status.ok()) {
     LOG(ERROR) << "Failed to collect APEX keys : " << status.error();
     return 1;
